2a2d2e465d0aa72e4e0b581a6c5d77c22d28bfa5
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-11-22 14:27:15 +0100

    setup starter app

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const slugify = require('slugify')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+const textSyncInstanceLocator = process.env.TEXT_SYNC_INSTANCE_LOCATOR
+const feedsInstanceLocator = process.env.FEEDS_INSTANCE_LOCATOR
+const feedsSecretKey = process.env.FEEDS_SECRET_KEY
+
+// create our Express app
+const app = express()
+
+// define folder to keep view files
+app.set('views', path.join(__dirname, 'views'))
+
+// use Pug as view engine
+app.set('view engine', 'pug')
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Home route
+app.get('/', async (req, res) => {
+  res.render('index')
+})
+
+// Show form for adding new post
+app.get('/posts/create', (req, res) => {
+  res.render('post_create')
+})
+
+// Process adding post
+app.post('/posts', async (req, res) => {})
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
